07b3e1f409e7bb4e617749e7888373c1cd7d29a6
Julien Roussel <roussel-julien@outlook.fr>
2025-02-25 16:37:49 +0100

    Slideshow gamme v1

--- src/pages/nos-gammes/NosGammes.jsx
+++ src/pages/nos-gammes/NosGammes.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router';
 import { Helmet } from 'react-helmet-async';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
 
 // CSS
 import './nosgammes.scss'
@@ -9,14 +11,26 @@ import './nosgammes.scss'
 import ButtonToggle from '../../components/button/ButtonToggle';
 import Button from '../../components/button/Button';
 
+const buttonStyle = {
+  width: "40px",
+  background: 'none',
+  border: '0px'
+};
+
+const properties = {
+  prevArrow: <button style={{ ...buttonStyle }}><img src="../../public/assets/picto/picto-back.svg" /></button>,
+  nextArrow: <button style={{ ...buttonStyle }}><img src="../../public/assets/picto/picto-next.svg" /></button>
+}
+
 const NosGammes = () => {
+
   return (
     <>
       <Helmet>
         <title>Nos gammes cosmétiques Évasion</title>
         <meta name='description' content="Évasion vous propose des produits cosmétiques et d'hygiène formulés à partir d'huiles essentielles 100% françaises, inspirés des régions les plus emblématiques de France." />
       </Helmet>
-      <section className="container">
+      <section className="container container-first">
           <h1 className='title-XH'>Nos gammes</h1>
       <div id="header-gammes" className='container container-grid'>
         <div className='container-image'>
@@ -44,6 +58,30 @@ const NosGammes = () => {
         </div>
       </div>
       <Button name="Découvrir nos produits" lien='/shop'/>
+      </section>
+      <section id="container-slider-gammes">
+        <Slide {...properties}>
+          <div className="each-slide slide-child slide-provence">
+              <div className="container container-grid">
+                  <span>Slide 1</span>
+              </div>
+          </div>
+          <div className="each-slide slide-child slide-corse">
+              <div className="container container-grid">
+                  <span>Slide 2</span>
+              </div>
+          </div>
+          <div className="each-slide slide-child slide-coteazur">
+              <div className="container container-grid">
+                  <span>Slide 3</span>
+              </div>
+          </div>
+          <div className="each-slide slide-child slide-bretagne">
+              <div className="container container-grid">
+                  <span>Slide 3</span>
+              </div>
+          </div>
+        </Slide>
       </section>
     </>
   )
